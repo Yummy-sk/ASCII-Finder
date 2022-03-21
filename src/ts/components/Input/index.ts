7e408bdacb457cf52dd07dc03b2bbfcef250b51e
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-03-21 22:48:12 +0900

    feat: 스켈레톤 ui

--- src/ts/components/Input/index.ts
+++ src/ts/components/Input/index.ts
@@ -2,11 +2,21 @@ import { setText, dispatch, getState, filterData } from '../../store';
 import { $ } from '../../utils';
 import { IData } from '../../types';
 
-function debounce(callback: (e: Event) => void, delay: number) {
+function debounce(
+  callback: (e: Event) => void,
+  delay: number,
+  $resultSkeleton: HTMLElement,
+  $resultWrapper: HTMLElement,
+  $notFound: HTMLElement
+) {
   let timerId: ReturnType<typeof setTimeout> | null;
+  return (e: any) => {
+    $resultWrapper.classList.remove('show');
+    $notFound.style.display = 'none';
 
-  return (e: Event) => {
-    console.log(e);
+    if (!e.target.value) $resultSkeleton.style.display = 'none';
+    else $resultSkeleton.style.display = 'block';
+
     if (timerId) clearTimeout(timerId);
     timerId = setTimeout(callback, delay, e);
   };
@@ -28,60 +38,67 @@ function changeState(value: string, data: Array<IData>) {
 export function Input() {
   const $input = $('input');
   const $resultWrapper = $('.result-wrapper');
+  const $resultSkeleton = $('.result-skeleton');
   const $notFound = $('.not-found');
 
   $input.addEventListener(
     'input',
-    debounce((e: any) => {
-      const {
-        fetch: { data },
-        form: { option },
-      } = getState();
-      const value = e.target.value;
-      $resultWrapper.classList.remove('show');
-      $notFound.style.display = 'none';
-
-      if (!option) {
-        e.target.value = '';
-        e.target.setAttribute('disabled', true);
-        e.target.setAttribute('placeholder', 'Please select option');
-        e.target.classList.toggle('error');
-      }
-
-      if (value && option) {
-        e.target.setAttribute('placeholder', 'Please Input ASCII Code');
-        changeState(value, data);
-
+    debounce(
+      (e: any) => {
         const {
-          fetch: { filtered },
+          fetch: { data },
+          form: { option },
         } = getState();
+        const value = e.target.value;
 
-        if (filtered) {
-          let html = '';
+        if (!option) {
+          e.target.value = '';
+          e.target.setAttribute('disabled', true);
+          e.target.setAttribute('placeholder', 'Please select option');
+          e.target.classList.toggle('error');
+        }
 
-          for (let filter in filtered as IData) {
-            if (filter !== 'Description' && filtered[filter]) {
-              html += `
+        if (value && option) {
+          e.target.setAttribute('placeholder', 'Please Input ASCII Code');
+          changeState(value, data);
+
+          $resultSkeleton.style.display = 'none';
+
+          const {
+            fetch: { filtered },
+          } = getState();
+
+          if (filtered) {
+            let html = '';
+
+            for (let filter in filtered as IData) {
+              if (filter !== 'Description' && filtered[filter]) {
+                html += `
               <div class="result-box">
                 <h3>${filter}</h3>
                 <p>${filtered[filter]}</p>
               </div>
             `;
+              }
             }
+
+            $resultWrapper.classList.add('show');
+            $resultWrapper.innerHTML = html;
+
+            return;
           }
 
-          $resultWrapper.classList.add('show');
-          $resultWrapper.innerHTML = html;
+          if (!filtered && value !== '') {
+            $notFound.style.display = 'block';
 
-          return;
+            return;
+          }
         }
-
-        if (!filtered && value !== '') {
-          $notFound.style.display = 'block';
-
-          return;
-        }
-      }
-    }, 500)
+      },
+      700,
+      $resultSkeleton,
+      $resultWrapper,
+      $notFound
+    )
   );
 }
