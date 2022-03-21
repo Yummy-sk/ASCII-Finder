6f08b72bbcb4509879e3e2cf1f72ef77bcb0406e
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-03-21 20:21:40 +0900

    feat: 디바운싱 적용

--- src/ts/components/Input/index.ts
+++ src/ts/components/Input/index.ts
@@ -1,7 +1,16 @@
 import { setText, dispatch, getState, filterData } from '../../store';
 import { $ } from '../../utils';
 import { IData } from '../../types';
-import { fetch } from '../../store/fetch';
+
+function debounce(callback: (e: Event) => void, delay: number) {
+  let timerId: ReturnType<typeof setTimeout> | null;
+
+  return (e: Event) => {
+    console.log(e);
+    if (timerId) clearTimeout(timerId);
+    timerId = setTimeout(callback, delay, e);
+  };
+}
 
 function changeState(value: string, data: Array<IData>) {
   const filtered = data.filter((data: any) => {
@@ -21,55 +30,58 @@ export function Input() {
   const $resultWrapper = $('.result-wrapper');
   const $notFound = $('.not-found');
 
-  $input.addEventListener('input', (e: any) => {
-    const {
-      fetch: { data },
-      form: { option },
-    } = getState();
-    const value = e.target.value;
-    $resultWrapper.classList.remove('show');
-    $notFound.style.display = 'none';
-
-    if (!option) {
-      e.target.value = '';
-      e.target.setAttribute('disabled', true);
-      e.target.setAttribute('placeholder', 'Please select option');
-      e.target.classList.toggle('error');
-    }
-
-    if (value && option) {
-      e.target.setAttribute('placeholder', 'Please Input ASCII Code');
-      changeState(value, data);
-
+  $input.addEventListener(
+    'input',
+    debounce((e: any) => {
       const {
-        fetch: { filtered },
+        fetch: { data },
+        form: { option },
       } = getState();
+      const value = e.target.value;
+      $resultWrapper.classList.remove('show');
+      $notFound.style.display = 'none';
 
-      if (filtered) {
-        let html = '';
+      if (!option) {
+        e.target.value = '';
+        e.target.setAttribute('disabled', true);
+        e.target.setAttribute('placeholder', 'Please select option');
+        e.target.classList.toggle('error');
+      }
 
-        for (let filter in filtered as IData) {
-          if (filter !== 'Description' && filtered[filter]) {
-            html += `
+      if (value && option) {
+        e.target.setAttribute('placeholder', 'Please Input ASCII Code');
+        changeState(value, data);
+
+        const {
+          fetch: { filtered },
+        } = getState();
+
+        if (filtered) {
+          let html = '';
+
+          for (let filter in filtered as IData) {
+            if (filter !== 'Description' && filtered[filter]) {
+              html += `
               <div class="result-box">
                 <h3>${filter}</h3>
                 <p>${filtered[filter]}</p>
               </div>
             `;
+            }
           }
+
+          $resultWrapper.classList.add('show');
+          $resultWrapper.innerHTML = html;
+
+          return;
         }
 
-        $resultWrapper.classList.add('show');
-        $resultWrapper.innerHTML = html;
+        if (!filtered && value !== '') {
+          $notFound.style.display = 'block';
 
-        return;
+          return;
+        }
       }
-
-      if (!filtered && value !== '') {
-        $notFound.style.display = 'block';
-
-        return;
-      }
-    }
-  });
+    }, 500)
+  );
 }
